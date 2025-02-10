4203e4edd332b1293f1ab84fff8b1f5b933fcbe7
ileostar <hi@leostar.top>
2025-02-10 19:12:39 +0800

    ⬆️ update deps

--- knip.config.ts
+++ knip.config.ts
@@ -1,7 +1,7 @@
 import type { KnipConfig } from 'knip'
 
 const config: KnipConfig = {
-  ignoreDependencies: ['@tailwindcss/typography', 'sharp', 'tailwindcss', 'react-email'],
+  ignoreDependencies: ['@tailwindcss/typography', 'sharp', 'tailwindcss', 'react-email', '@types/next-pwa'],
   entry: ['*.config.mjs', '*.config.mts'],
   project: ['src/**/*.{ts,tsx}', 'scripts/*.ts'],
   ignore: ['./src/db/seed.ts', '**/*.d.ts', './src/components/base/*', './src/plugins/*'],
