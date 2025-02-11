4964a54281815daa9a2411e39a82cea68b723980
ileostar <hi@leostar.top>
2025-02-11 10:34:25 +0800

    🔍 update sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,9 +1,19 @@
 import type { MetadataRoute } from 'next'
-
+import { allPosts, allProjects } from 'content-collections'
 import { SITE_URL } from '~/config/constants'
 
-function sitemap(): MetadataRoute.Sitemap {
-  const routes = ['', '/blog', '/guestbook', '/albums', '/projects', '/about']
+const sitemap = (): MetadataRoute.Sitemap => {
+  const routes = [
+    '',
+    '/blog',
+    '/projects',
+    '/links',
+    '/guestbook',
+    '/albums',
+    '/about',
+    ...new Set(allProjects.map((project) => `/projects/${project.slug}`)),
+    ...new Set(allPosts.map((post) => `/blog/${post.slug}`))
+  ]
 
   return routes.map(route => ({
     url: `${SITE_URL}${route}`,
