8f5c40685f5d40f4f98b140e06d648899a437855
ileostar <hi@leostar.top>
2025-03-02 01:01:00 +0800

    🚨 remove lint warning

--- src/components/infinite-scrolling-loading.tsx
+++ src/components/infinite-scrolling-loading.tsx
@@ -11,33 +11,33 @@ const InfiniteScrollingLoading: React.FC<InfiniteScrollingLoadingProps> = ({ has
     <>
       {hasNextPage
         ? (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            className="inline-flex items-center gap-2 text-gray-500 dark:text-gray-400"
-          >
-            <svg
-              className="animate-spin h-6 w-6 text-pink-500"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="currentColor"
-                d="M12 4V2A10 10 0 0 0 2 12h2a8 8 0 0 1 8-8z"
-              />
-            </svg>
-          </motion.div>
-        )
-        : (
-          status === 'success' && (
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
-              className="text-gray-500"
+              className="inline-flex items-center gap-2 text-gray-500 dark:text-gray-400"
             >
-              {totalItems > 0 ? '🎉 已经到底啦 (≧▽≦) ～' : '⚠️ 暂无相关数据 (╯︵╰,)'}
+              <svg
+                className="animate-spin h-6 w-6 text-pink-500"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M12 4V2A10 10 0 0 0 2 12h2a8 8 0 0 1 8-8z"
+                />
+              </svg>
             </motion.div>
           )
-        )}
+        : (
+            status === 'success' && (
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                className="text-gray-500"
+              >
+                {totalItems > 0 ? '🎉 已经到底啦 (≧▽≦) ～' : '⚠️ 暂无相关数据 (╯︵╰,)'}
+              </motion.div>
+            )
+          )}
     </>
   )
 }
