f3bc4644470aab0aa8a4bf24d3a16310b73f0fbb
eonova <hi@eonova.me>
2025-12-05 01:48:27 +0800

    🐛 try to build error

--- src/app/(page)/layout.tsx
+++ src/app/(page)/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata, Viewport } from 'next'
+import type { Viewport } from 'next'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { GeistMono } from 'geist/font/mono'
 import { GeistSans } from 'geist/font/sans'
@@ -6,13 +6,6 @@ import localFont from 'next/font/local'
 import SignInDialog from '~/components/layouts/sign-in-dialog'
 import Hello from '~/components/shared/hello'
 import { ErrorBoundaryMonitor, PerformanceMonitor } from '~/components/shared/performance-monitor'
-import {
-  SITE_DESCRIPTION,
-  SITE_KEYWORDS,
-  SITE_NAME,
-  SITE_TITLE,
-  SITE_URL,
-} from '~/config/constants'
 import { cn } from '~/utils'
 import Providers from '../providers'
 import '~/styles/globals.css'
@@ -21,88 +14,6 @@ const DingTalk = localFont({
   src: '../../../public/fonts/DingTalk.otf',
   variable: '--font-dingtalk',
 })
-
-export const metadata: Metadata = {
-  metadataBase: new URL(SITE_URL),
-  title: {
-    default: SITE_TITLE,
-    template: `%s | ${SITE_TITLE}`,
-  },
-  description: SITE_DESCRIPTION,
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
-      'index': true,
-      'follow': true,
-      'max-video-preview': -1,
-      'max-image-preview': 'large',
-      'max-snippet': -1,
-    },
-  },
-  manifest: '/favicon/site.webmanifest',
-  twitter: {
-    card: 'summary_large_image',
-    title: SITE_NAME,
-    description: SITE_DESCRIPTION,
-    site: '@030Eonova',
-    siteId: '1693931091722465280',
-    creator: '@030Eonova',
-    creatorId: '1693931091722465280',
-    images: [
-      {
-        url: '/images/og.png',
-        width: 1200,
-        height: 630,
-        alt: SITE_DESCRIPTION,
-      },
-    ],
-  },
-  keywords: SITE_KEYWORDS,
-  creator: 'eonova',
-  openGraph: {
-    url: SITE_URL,
-    type: 'website',
-    title: SITE_TITLE,
-    siteName: SITE_TITLE,
-    description: SITE_DESCRIPTION,
-    locale: 'en-US',
-    images: [
-      {
-        url: '/images/og.png',
-        width: 1200,
-        height: 630,
-        alt: SITE_DESCRIPTION,
-        type: 'image/png',
-      },
-    ],
-  },
-  icons: {
-    icon: '/favicon/favicon.svg',
-    shortcut: '/favicon/favicon.svg',
-    apple: [
-      {
-        url: '/favicon/apple-touch-icon.png',
-        sizes: '180x180',
-        type: 'image/png',
-      },
-    ],
-    other: [
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '16x16',
-        url: '/favicon/favicon-16x16.png',
-      },
-      {
-        rel: 'icon',
-        type: 'image/png',
-        sizes: '32x32',
-        url: '/favicon/favicon-32x32.png',
-      },
-    ],
-  },
-}
 
 export const viewport: Viewport = {
   themeColor: [
---
+++ src/lib/metadata.ts
@@ -0,0 +1,111 @@
+import type { Metadata } from 'next'
+
+import { deepmerge } from '@fastify/deepmerge'
+
+import { MY_NAME, OG_IMAGE_HEIGHT, OG_IMAGE_TYPE, OG_IMAGE_WIDTH, SITE_DESCRIPTION, SITE_NAME } from '~/config/constants'
+
+import { getBaseUrl } from '~/utils/get-base-url'
+
+type Options = {
+  root?: boolean
+  pathname?: string
+  title: string
+  description: string
+  locale: string
+} & Partial<Metadata>
+
+export function createMetadata(options: Options): Metadata {
+  const { root = false, pathname, title, description, locale, ...rest } = options
+  const baseUrl = getBaseUrl()
+
+  const resolvedTitle = root ? title : `${title} | ${MY_NAME}`
+  const resolvedOGImageUrl = pathname ? `/og${pathname}/image.webp` : '/og/image.webp'
+
+  const currentUrl = `${baseUrl}${resolvedOGImageUrl}`
+
+  return deepmerge()(
+    {
+      title: resolvedTitle,
+      description,
+      creator: MY_NAME,
+      manifest: `${baseUrl}/site.webmanifest`,
+      alternates: {
+        canonical: currentUrl,
+      },
+      robots: {
+        index: true,
+        follow: true,
+        googleBot: {
+          'index': true,
+          'follow': true,
+          'max-video-preview': -1,
+          'max-image-preview': 'large',
+          'max-snippet': -1,
+        },
+      },
+      authors: {
+        name: MY_NAME,
+        url: baseUrl,
+      },
+      openGraph: {
+        title: resolvedTitle,
+        description,
+        url: currentUrl,
+        siteName: MY_NAME,
+        type: 'website',
+        locale,
+        images: [
+          {
+            url: resolvedOGImageUrl,
+            width: OG_IMAGE_WIDTH,
+            height: OG_IMAGE_HEIGHT,
+            type: OG_IMAGE_TYPE,
+          },
+        ],
+      },
+      twitter: {
+        card: 'summary_large_image',
+        title: SITE_NAME,
+        description: SITE_DESCRIPTION,
+        site: '@030Eonova',
+        siteId: '1693931091722465280',
+        creator: '@030Eonova',
+        creatorId: '1693931091722465280',
+        images: [
+          {
+            url: '/images/og.png',
+            width: 1200,
+            height: 630,
+            alt: SITE_DESCRIPTION,
+          },
+        ],
+      },
+      icons: {
+        icon: '/favicon/favicon.svg',
+        shortcut: '/favicon/favicon.svg',
+        apple: [
+          {
+            url: '/favicon/apple-touch-icon.png',
+            sizes: '180x180',
+            type: 'image/png',
+          },
+        ],
+        other: [
+          {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '16x16',
+            url: '/favicon/favicon-16x16.png',
+          },
+          {
+            rel: 'icon',
+            type: 'image/png',
+            sizes: '32x32',
+            url: '/favicon/favicon-32x32.png',
+          },
+        ],
+      },
+    },
+    rest,
+  )
+}
