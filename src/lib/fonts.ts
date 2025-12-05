224fc333bd70735093532f6c1baff419892a6d65
eonova <hi@eonova.me>
2025-12-06 07:36:58 +0800

    🐛 fix og vercel build error

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -7,70 +7,22 @@ import { cache } from 'react'
 
 const getFontPath = (fontName: string) => path.join(process.cwd(), 'public', 'fonts', fontName)
 
-const getRegularFont = cache(async () => {
-  const response = await fs.readFile(getFontPath('Geist-Regular.otf'))
-  const font = Uint8Array.from(response).buffer
-
-  return font
-})
-
-const getMediumFont = cache(async () => {
-  const response = await fs.readFile(getFontPath('Geist-Medium.otf'))
-  const font = Uint8Array.from(response).buffer
-
-  return font
-})
-
-const getSemiBoldFont = cache(async () => {
-  const response = await fs.readFile(getFontPath('Geist-SemiBold.otf'))
-  const font = Uint8Array.from(response).buffer
-
-  return font
-})
-
-const getFallbackFont = cache(async () => {
-  const response = await fs.readFile(getFontPath('DingTalk.otf'))
+const getHappyWorldFont = cache(async () => {
+  const response = await fs.readFile(getFontPath('HappyWorld.otf'))
   const font = Uint8Array.from(response).buffer
 
   return font
 })
 
 export async function getOGImageFonts(_title: string): Promise<SatoriOptions['fonts']> {
-  const [regularFontData, mediumFontData, semiBoldFontData, fallbackFontData] = await Promise.all([
-    getRegularFont(),
-    getMediumFont(),
-    getSemiBoldFont(),
-    getFallbackFont(),
+  const [happyWorldFontData] = await Promise.all([
+    getHappyWorldFont(),
   ])
 
   return [
     {
-      name: 'Geist Sans',
-      data: regularFontData,
-      style: 'normal',
-      weight: 400,
-    },
-    {
-      name: 'Geist Sans',
-      data: mediumFontData,
-      style: 'normal',
-      weight: 500,
-    },
-    {
-      name: 'Geist Sans',
-      data: semiBoldFontData,
-      style: 'normal',
-      weight: 600,
-    },
-    {
-      name: 'Noto Sans TC',
-      data: fallbackFontData,
-      style: 'normal',
-      weight: 400,
-    },
-    {
       name: 'Noto Sans SC',
-      data: fallbackFontData,
+      data: happyWorldFontData,
       style: 'normal',
       weight: 400,
     },
